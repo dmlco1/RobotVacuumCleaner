39e1abeb5ccc34cabd6195b952f912caa18fb406
dmlco1 <dmlco1@iscte-iul.pt>
2021-11-07 16:39:01 +0000

    done

--- src/VacuumCleaner.java
+++ src/VacuumCleaner.java
@@ -1,5 +1,37 @@
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+
+import java.util.Scanner;
+
 public class VacuumCleaner {
     public static void main(String[] args) {
 
+        Simulator robot = new Simulator();
+
+        String filename = "fcls/control.fcl";
+        FIS fis = FIS.load(filename, true);
+        if (fis == null) {
+            System.err.println("Can't load file: '" + filename + "'");
+            System.exit(1);
+        }
+
+        while (true) {
+            // default function block
+            FunctionBlock fb = fis.getFunctionBlock(null);
+
+            // set the inputs
+            fb.setVariable("leftSensor", robot.getDistanceL());
+            fb.setVariable("centerSensor", robot.getDistanceC());
+            fb.setVariable("rightSensor", robot.getDistanceR());
+            // evaluate
+            fb.evaluate();
+
+            // show output variable's chart
+            fb.getVariable("action").defuzzify();
+            double newAngle = fb.getVariable("action").getValue();
+
+            robot.setRobotAngle(newAngle);
+            robot.step();
+        }
     }
 }
